5785e57351e552fbd2856d5edb79d68c32a9803e
Vladimira <vladimirakolar@gmail.com>
2022-01-20 19:11:50 +0100

    Added skill controler

---
+++ API/Controllers/SkillsController.cs
@@ -0,0 +1,42 @@
+﻿using API.ViwModels;
+using AutoMapper;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly ISkillService _skillService;
+        private readonly IMapper _mapper;
+
+        public SkillsController(ISkillService skillService, IMapper mapper)
+        {
+            _skillService = skillService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<IList<SkillViwModel>>> GetAllSkillAysinc()
+        {
+            var skills = await _skillService.GetSkillsAsync();
+
+            var result = _mapper.Map<IList<SkillViwModel>>(skills);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SkillViwModel>> GetSkillByIdAysinc(int id)
+        {
+            var skill = await _skillService.GetSkillAsync(id);
+            var result = _mapper.Map<SkillViwModel>(skill);
+
+            return Ok(result);
+        }
+    }
+}
